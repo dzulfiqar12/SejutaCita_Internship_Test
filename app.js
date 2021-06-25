3e3733b4922d4e526b813efde243666bb7b666d4
dzulfiqar12 <78007781+dzulfiqar12@users.noreply.github.com>
2021-06-25 17:20:17 +0700

    add JWT error handler

--- app.js
+++ app.js
@@ -33,18 +33,30 @@ app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
-//Error handler for DB
+//Error handler if error happen in DB or JWT
 app.use((err, req, res, next) => {
   let error = { ...err };
+  console.log(err);
   if (error.name === 'CastError') {
-    next(new AppError('Failed to casting', 404));
+    next(new AppError('Failed to casting', 400));
   }
   if (error.code === 11000) {
-    next(new AppError('Duplicate field', 404));
+    next(new AppError('Duplicate field in email or username ', 400));
   }
-  if (error.name === 'ValidationError')
-    next(new AppError('Validation error', 404));
+  if (error.name === 'ValidationError') {
+    next(new AppError('Validation error', 400));
+  }
+
+  if (error.name === 'JsonWebTokenError') {
+    next(new AppError('Invalid token. Please log in again!', 401));
+  }
+  if (error.name === 'TokenExpiredError') {
+    next(new AppError('Your token has expired! Please log in again.', 401));
+  }
+
+  next(err);
 });
+
 //Send error to user
 app.use(sendError);
 module.exports = app;
